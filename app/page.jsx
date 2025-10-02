9f186d11437a8523d57896c0d2d8ebad57555ddf
shahid <shahidazhari313@gmail.com>
2025-10-02 09:50:33 +0530

    added logo in loader

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import Image from "next/image"; // ✅ Import Next.js optimized Image
 import { Header } from "../components/Header";
 import { Hero } from "../components/Hero";
 import { About } from "../components/About";
@@ -20,8 +21,18 @@ export default function HomePage() {
         return (
             <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-[#e0f2ff] to-[#f0f9ff]">
                 <div className="relative flex items-center justify-center w-28 h-28">
-                    <div className="absolute w-28 h-28 rounded-full border-4 border-[#0c448d]/10 border-t-[#038b8b] animate-spin-slow shadow-lg"></div>
-                    <span className="text-6xl font-semibold font-serif text-[#0c448d] animate-pulse-fast">S</span>
+                    {/* Spinner border */}
+                    <div className="absolute w-28 h-28 rounded-full border-4 bg-[#038b8b]/5 border-[#0c448d]/10 border-t-[#038b8b] animate-spin-slow shadow-lg"></div>
+                    
+                    {/* Logo Image instead of "S" */}
+                    <Image
+                        src="/favicon.ico"
+                        alt="Logo"
+                        width={50}
+                        height={50}
+                        className="rounded-2xl shadow-md animate-pulse-fast  bg-white"
+                        priority
+                    />
                 </div>
                 <span className="mt-3 text-2xl sm:text-4xl font-semibold text-[#0c448d]/70 tracking-widest animate-fade-in uppercase">
                     Statheros Health Care
